3546579624195a4ce70ef811a8e99647e7d72244
Jan De Rydt <jan.rydt@northumbria.ac.uk>
2024-05-07 12:46:50 +0100

    catch exception for Boundary conditions at restart

--- ANT_Inverse/ANT_Inverse_9999/DefineBoundaryConditions.m
+++ ANT_Inverse/ANT_Inverse_9999/DefineBoundaryConditions.m
@@ -3,27 +3,36 @@ function  BCs=DefineBoundaryConditions(UserVar,CtrlVar,MUA,BCs,time,s,b,h,S,B,ub
 %%
 persistent AA BB
 
-if ~isempty(UserVar.BaseMesh.FixedBoundaryPoints)
+if isfield(UserVar,'BaseMesh')
+    
+    if ~isempty(UserVar.BaseMesh.FixedBoundaryPoints)
 
-    if isempty(AA)
-      
-        % load points that define the line-segments along which the BCs are to be defined
-        load(UserVar.BaseMesh.FixedBoundaryPoints);
-        AA=[xOuter(1:end-1) yOuter(1:end-1)] ; BB=[xOuter(2:end) yOuter(2:end)];
-    
+        if isempty(AA)
+          
+            % load points that define the line-segments along which the BCs are to be defined
+            load(UserVar.BaseMesh.FixedBoundaryPoints);
+            AA=[xOuter(1:end-1) yOuter(1:end-1)] ; BB=[xOuter(2:end) yOuter(2:end)];
+        
+        end
+        
+        % find all boundary nodes within 1m distance from the line segment.
+        x=MUA.coordinates(:,1);  y=MUA.coordinates(:,2); tolerance=1;
+        I = DistanceToLineSegment([x(MUA.Boundary.Nodes) y(MUA.Boundary.Nodes)],AA,BB,tolerance);
+        
+        BCs.vbFixedNode=MUA.Boundary.Nodes(I);
+        BCs.ubFixedNode=MUA.Boundary.Nodes(I);
+        %BCs.vbFixedNode=MUA.Boundary.Nodes(I);
+        %BCs.ubFixedNode=MUA.Boundary.Nodes(I);
+        
+        BCs.ubFixedValue=BCs.ubFixedNode*0;
+        BCs.vbFixedValue=BCs.vbFixedNode*0;
+        
     end
-    
-    % find all boundary nodes within 1m distance from the line segment.
-    x=MUA.coordinates(:,1);  y=MUA.coordinates(:,2); tolerance=1;
-    I = DistanceToLineSegment([x(MUA.Boundary.Nodes) y(MUA.Boundary.Nodes)],AA,BB,tolerance);
-    
-    BCs.vbFixedNode=MUA.Boundary.Nodes(I);
-    BCs.ubFixedNode=MUA.Boundary.Nodes(I);
-    %BCs.vbFixedNode=MUA.Boundary.Nodes(I);
-    %BCs.ubFixedNode=MUA.Boundary.Nodes(I);
-    
-    BCs.ubFixedValue=BCs.ubFixedNode*0;
-    BCs.vbFixedValue=BCs.vbFixedNode*0;
+
+else
+
+    fprintf("DefineBoundaryConditions did not adjust BCs.\n");
+
 end
 
 end
