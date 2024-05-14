84dc59dabcd957ec4846e4c61c29a78509155357
Jan De Rydt <jan.rydt@northumbria.ac.uk>
2024-05-14 12:27:32 +0100

    update to mesh filenames

--- ANT_DefineBaseMesh.m
+++ ANT_DefineBaseMesh.m
@@ -5,16 +5,16 @@ UserVar.BaseMesh.FixedBoundaryPoints = '';
 switch char(BaseMesh)
     case {'2000_meshmin5000_meshmax100000','2000_meshmin1500_meshmax100000'}
         % adjust and copy mesh files  
-        UserVar.BaseMesh.BCs = "../ANT_Data/ANT_Ua_BaseMeshGeneration/ANT_meshboundarycoordinates_"+BaseMesh+"_extrudemesh0_variableboundaryres1";
-        UserVar.BaseMesh.Mesh = "../ANT_Data/ANT_Ua_BaseMeshGeneration/ANT_basemesh_"+BaseMesh+"_extrudemesh0_variableboundaryres1";
+        UserVar.BaseMesh.BCs = "../ANT_Data/ANT_Ua_BaseMeshGeneration/ANT_meshboundarycoordinates_"+BaseMesh+"_extrudemesh0_variableboundaryres1.mat";
+        UserVar.BaseMesh.Mesh = "../ANT_Data/ANT_Ua_BaseMeshGeneration/ANT_basemesh_"+BaseMesh+"_extrudemesh0_variableboundaryres1.mat";
     case {'2000_2009_2014_2018_meshmin3000_meshmax100000'}
 	% adjust and copy mesh files
-	    UserVar.BaseMesh.BCs = "../ANT_Data/ANT_Ua_BaseMeshGeneration/ANT_meshboundarycoordinates_"+BaseMesh+"_extrudemesh1_variableboundaryres1";
-        UserVar.BaseMesh.Mesh = "../ANT_Data/ANT_Ua_BaseMeshGeneration/ANT_basemesh_"+BaseMesh+"_extrudemesh1_variableboundaryres1";
+	    UserVar.BaseMesh.BCs = "../ANT_Data/ANT_Ua_BaseMeshGeneration/ANT_meshboundarycoordinates_"+BaseMesh+"_extrudemesh1_variableboundaryres1.mat";
+        UserVar.BaseMesh.Mesh = "../ANT_Data/ANT_Ua_BaseMeshGeneration/ANT_basemesh_"+BaseMesh+"_extrudemesh1_variableboundaryres1.mat";
     case {'2000_2009_2014_2018_meshmin3000_meshmax100000_refined'}
 	% adjust and copy mesh files
-	    UserVar.BaseMesh.BCs = "../ANT_Data/ANT_Ua_BaseMeshGeneration/ANT_meshboundarycoordinates_"+BaseMesh+"_extrudemesh1_variableboundaryres1";
-        UserVar.BaseMesh.Mesh = "../ANT_Data/ANT_Ua_BaseMeshGeneration/ANT_basemesh_"+BaseMesh+"_extrudemesh1_variableboundaryres1";
+	    UserVar.BaseMesh.BCs = "../ANT_Data/ANT_Ua_BaseMeshGeneration/ANT_meshboundarycoordinates_"+BaseMesh+"_extrudemesh1_variableboundaryres1.mat";
+        UserVar.BaseMesh.Mesh = "../ANT_Data/ANT_Ua_BaseMeshGeneration/ANT_basemesh_"+BaseMesh+"_extrudemesh1_variableboundaryres1.mat";
     case {'AS_PROPHET_2000'}
     % adjust and copy mesh files
         froot = "/mnt/md0/Ua/cases/ASE_Inversions/";
