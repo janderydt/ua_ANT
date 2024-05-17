f12e4f0113aa23a74fbb55e97e1dfafae5a8bf4b
Jan De Rydt <jan.rydt@northumbria.ac.uk>
2024-05-17 14:04:08 +0100

    change max number of simulteneous runs

--- ua_jobs_master.py
+++ ua_jobs_master.py
@@ -90,7 +90,7 @@ if __name__ == "__main__":
     # cpu count
     cpus = os.cpu_count()
 
-    while run_counter<=15:
+    while run_counter<=10:
    
         runtable = read_runinfo('RunTable.csv',runtype)
 	 
