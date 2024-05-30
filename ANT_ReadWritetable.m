a10a7550991f82032ad3416c29904a44cda19134
Jan De Rydt <jan.rydt@northumbria.ac.uk>
2024-05-30 10:27:13 +0100

    cosmetics

--- ANT_ReadWritetable.m
+++ ANT_ReadWritetable.m
@@ -1,6 +1,7 @@
 function RunTable = ANT_ReadWritetable(UserVar,RunTable,mode)
 
 isactive = 1;
+
 while isactive
 
     pause(1);
@@ -30,7 +31,7 @@ switch mode
         % step 1. read table and find lines in both old and new tables
         ind_new = find(RunTable{:,'ExpID'}(:) == UserVar.ExpID);
         opts = setoptions(UserVar);
-	RunTable_old = readtable(UserVar.Table,opts);
+	    RunTable_old = readtable(UserVar.Table,opts);
         ind_old = find(RunTable_old{:,'ExpID'}(:) == UserVar.ExpID);
         % step 2. replace 1 line and write
         RunTable_old(ind_old,:) = RunTable(ind_new,:);
@@ -50,36 +51,26 @@ function opts = setoptions(UserVar)
 opts = detectImportOptions(UserVar.Table);
 
 if UserVar.type == "Inverse"
-                I=find(contains(opts.VariableNames,'Domain'));
-                opts.VariableTypes(I)={'char'};
-                I=find(contains(opts.VariableNames,'InverseIterations'));
-                opts.VariableTypes(I)={'char'};
-                I=find(contains(opts.VariableNames,'InverseIterationsDone'));
-                opts.VariableTypes(I)={'double'};
-                I=find(contains(opts.VariableNames,'SpinupYears'));
-                opts.VariableTypes(I)={'char'};
-                I=find(contains(opts.VariableNames,'SpinupYearsDone'));
-                opts.VariableTypes(I)={'double'};
-                I=find(contains(opts.VariableNames,'Comments'));
-                opts.VariableTypes(I)={'char'};
+
+    I=find(contains(opts.VariableNames,'Domain'));
+    opts.VariableTypes(I)={'char'};
+    I=find(contains(opts.VariableNames,'InverseIterations'));
+    opts.VariableTypes(I)={'char'};
+    I=find(contains(opts.VariableNames,'InverseIterationsDone'));
+    opts.VariableTypes(I)={'double'};
+    I=find(contains(opts.VariableNames,'SpinupYears'));
+    opts.VariableTypes(I)={'char'};
+    I=find(contains(opts.VariableNames,'SpinupYearsDone'));
+    opts.VariableTypes(I)={'double'};
+    I=find(contains(opts.VariableNames,'Comments'));
+    opts.VariableTypes(I)={'char'};
                 
-            elseif UserVar.type == "Diagnostic"
-            	% none to correct
+elseif UserVar.type == "Diagnostic"
+    % none to correct
 
-            elseif UserVar.type == "Transient"
-            	% none to correct
+elseif UserVar.type == "Transient"
+    % none to correct
 
-            end
-            
-            %I=find(contains(opts.VariableNames,'SubmissionTime'));
-            %opts.VariableTypes(I)={'char'};
-            %opts = setvaropts(opts,'SubmissionTime','InputFormat','dd/MM/yyyy HH:mm:ss'); 
+end
 
-            %I=find(contains(opts.VariableNames,'ErrorTime'));
-            %opts.VariableTypes(I)={'char'};
-            %opts = setvaropts(opts,'ErrorTime','InputFormat','dd/MM/yyyy HH:mm:ss'); 
-
-            %I=find(contains(opts.VariableNames,'FinishedTime'));
-            %opts.VariableTypes(I)={'char'};
-            %opts = setvaropts(opts,'FinishedTime','InputFormat','dd/MM/yyyy HH:mm:ss'); 
 end
