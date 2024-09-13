26508ebb0b2a8b0365b11c3c392b43ca6c8698da
Jan De Rydt <jan.rydt@northumbria.ac.uk>
2024-09-13 17:54:21 +0100

    remove spurious files

--- ANT_Transient/cases/ANT_nsmbl_Transient_814/DefineBoundaryConditions.m
+++
@@ -1,7 +0,0 @@
-function  BCs=DefineBoundaryConditions(UserVar,CtrlVar,MUA,BCs,time,s,b,h,S,B,ub,vb,ud,vd,GF)
-
-%%
-% none required
-
-
-end
--- ANT_Transient/cases/ANT_nsmbl_Transient_814/DefineMassBalance.m
+++
@@ -1,7 +0,0 @@
-function [UserVar,as,ab]=DefineMassBalance(UserVar,CtrlVar,MUA,time,s,b,h,S,B,rho,rhow,GF)
-
-as = 0*s;
-ab = 0*s;
-
-end
-
